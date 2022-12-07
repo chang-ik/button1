48d436cdfc3718a572670ac3441bbf1d5aac2fc0
changho oh <dhckdgh38@naver.com>
2022-12-07 13:13:45 +0900

    V1

--- button1.py
+++ button1.py
@@ -3,7 +3,7 @@ from tkinter import *
 window =Tk()
 
 b1 = Button(window, text="changho Oh")
-b2 = Button(window, text="60192216sss")
+b2 = Button(window, text="60192216")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
